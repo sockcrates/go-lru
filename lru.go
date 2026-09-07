64c65f1e002472de44ab8cb8f86dceace4d821ab
sockcrates <17348049+sockcrates@users.noreply.github.com>
2026-09-07 09:57:15 -0400

    feat: implement LRU cache

--- lru.go
+++ lru.go
@@ -1,16 +1,50 @@
 package lru
 
+import (
+	"container/list"
+)
+
+type entry[K comparable, V any] struct {
+	key   K
+	value V
+}
+
 type LRU[K comparable, V any] struct {
-	_ struct{}
+	cache    map[K]*list.Element
+	Capacity int
+	list     *list.List
+	_        struct{}
 }
 
 func New[K comparable, V any](capacity int) *LRU[K, V] {
-	return &LRU[K, V]{}
+	return &LRU[K, V]{cache: make(map[K]*list.Element), Capacity: capacity, list: list.New()}
 }
 
-func (l *LRU[K, V]) Put(key K, value V) {}
+func (l *LRU[K, V]) Put(key K, value V) {
+	if e, ok := l.cache[key]; ok {
+		e.Value = entry[K, V]{key: key, value: value}
+		l.list.MoveToFront(e)
+		return
+	}
+	e := l.list.PushFront(entry[K, V]{key: key, value: value})
+	l.cache[key] = e
+	if l.list.Len() > l.Capacity {
+		lru := l.list.Back()
+		if lru == nil {
+			return
+		}
+		data := lru.Value
+		delete(l.cache, data.(entry[K, V]).key)
+		l.list.Remove(lru)
+	}
+}
 
 func (l *LRU[K, V]) Get(key K) (V, bool) {
-	var zero V
-	return zero, false
+	d, ok := l.cache[key]
+	if !ok {
+		var zero V
+		return zero, false
+	}
+	l.list.MoveToFront(d)
+	return d.Value.(entry[K, V]).value, true
 }
